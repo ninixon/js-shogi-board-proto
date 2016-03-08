d12c7a16a9ad772ff535e2a306038207a25a2ba0
Haruka Kai <plalal@gmail.com>
2016-03-09 01:54:43 +0900

    board

--- src/board-impl.js
+++ src/board-impl.js
@@ -1,5 +1,52 @@
 function () {
 	var board = function(o) {
+		this["pcs0"] = [];
+		this["pcs1"] = [];
+		this["ps0"] = {};
+		this["ps1"] = {};
+	};
+
+	var is_array = function(a) {
+		return Object.prototype.toString.call(a) === '[object Array]';
+	};
+
+	var array_deepcopy = function(a) {
+		var b = [];
+		for (var i = 0; i < a.length(); ++i) {
+			var e = a[i];
+			b[i] = is_array(e) ? array_deepcopy(e) : e;
+		}
+		return b;
+	};
+
+	var pskeys =
+	[ "FU"
+	, "KY"
+	, "KE"
+	, "GI"
+	, "KI"
+	, "KA"
+	, "HI"
+	];
+
+	var histogram_copy = function(a) {
+		var b = {];
+		for (var i = 0; i < pskeys.length(); ++i) {
+			var k = pskeys[i];
+			b[k] = a[k] || 0;
+		}
+		return b;
+	};
+
+	board.prototype.to_data = function() {
+		var b = this;
+		var r = {};
+		r["pcs0"] = array_deepcopy(b["pcs0"]);
+		r["pcs1"] = array_deepcopy(b["pcs1"]);
+		r["ps0"] = histogram_copy(b["ps0"]);
+		r["ps1"] = histogram_copy(b["ps1"]);
+		// TODO: hllist
+		return r;
 	};
 
 	return board;
