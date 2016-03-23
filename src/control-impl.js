3950fec9bebb37086f92d2d978f5cd9c1805f73c
Haruka Kai <plalal@gmail.com>
2016-03-24 06:04:41 +0900

    module

--- src/control-impl.js
+++ src/control-impl.js
@@ -3,7 +3,7 @@ function () {
 	var SVGView = (function(def) { return def(); })(
 #		include "viewer-svg-impl.js"
 	);
-	var BoardModel = (function(def) { return def(); })({
+	var BoardModel = (function(def) { return def(); })(
 #		include "board-impl.js"
 	);
 	control['SVGView'] = SVGView;
