d40adaa72e0ea25361da3723ad9bf81b4b0d1631
Haruka Kai <plalal@gmail.com>
2016-03-26 23:57:53 +0900

    parser included

--- src/control-impl.js
+++ src/control-impl.js
@@ -6,6 +6,9 @@ function () {
 	var BoardModel = (function(def) { return def(); })(
 #		include "board-impl.js"
 	);
+	var Parser = (function(def) { return def(); })(
+#		include "parser-kif-impl.js"
+	);
 	control['SVGView'] = SVGView;
 	return control;
 }
